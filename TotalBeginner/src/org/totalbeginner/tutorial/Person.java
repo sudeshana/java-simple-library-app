d92083c64716a20b1f4048ebb08ee13b11593d4c
Sudeshana Ariyasena <dgpradeepal@gmail.com>
2017-10-15 12:05:52 +0530

    starting simple app

---
+++ TotalBeginner/src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,14 @@
+package org.totalbeginner.tutorial;
+
+public class Person {
+
+	private String name;
+	private int maximumBooks;
+	
+	// constructors
+	public Person(){
+		
+		name = "known name";
+		maximumBooks = 3;
+	}
+}
