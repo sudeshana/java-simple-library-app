b021267a1974ebbd003b079508568a334f507414
Sudeshana Ariyasena <dgpradeepal@gmail.com>
2017-11-12 19:42:30 +0530

    Added JUnit Test Case
    Added Person Test Case

--- TotalBeginner/src/org/totalbeginner/tutorial/Person.java
+++ TotalBeginner/src/org/totalbeginner/tutorial/Person.java
@@ -8,7 +8,7 @@ public class Person {
 	// constructors
 	public Person(){
 		
-		name = "known name";
+		name = "unknown name";
 		maximumBooks = 3;
 	}
 	
---
+++ TotalBeginner/test/org/totalbeginner/tutorial/PersonTest.java
@@ -0,0 +1,30 @@
+package org.totalbeginner.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void testPerson() {
+		Person p1 = new Person();
+		assertEquals("unknown name",p1.getName());
+		assertEquals(3,p1.getMaximumBooks());
+	}
+
+	@Test
+	public void testSetName() {
+		Person p2 = new Person();
+		p2.setName("Fred");
+		assertEquals("Fred",p2.getName());
+	}
+
+	@Test
+	public void testSetMaximumBooks() {
+		Person p3 = new Person();
+		p3.setMaximumBooks(10);;
+		assertEquals(10,p3.getMaximumBooks());
+	}
+
+}
